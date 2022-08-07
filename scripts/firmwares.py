fc416e6edca15aa4339e22b7f958d1dc08bdb018
PoloNX <geyrecharlie@gmail.com>
2022-08-07 23:13:15 +0000

    fix firmware

--- scripts/firmwares.py
+++ scripts/firmwares.py
@@ -1,3 +1,4 @@
+import re
 from basemodule import BaseModule
 
 class Firmwares(BaseModule):
@@ -14,7 +15,7 @@ class Firmwares(BaseModule):
     def handle_module(self):
         for i in range(len(self.config)):
             release = self.get_releases(i)
-            for j in range(len(release)):
+            for j in range(release.totalCount):
                 assets = self.get_asset_links(release[j], i)
                 for asset in assets:
                     self.out[release[j].title] = asset.browser_download_url
