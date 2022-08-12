8e5d032f833677f7c98420e5f905c3934472bcf9
PoloNX <geyrecharlie@gmail.com>
2022-08-12 21:23:20 +0000

    remove changelog :p

--- scripts/app.py
+++ scripts/app.py
@@ -16,5 +16,4 @@ class App(BaseModule):
         asset = self.get_asset_link(release, self.config[0]["assetPatterns"][0])
         self.out["AtmoPackUpdater"] = asset[0].browser_download_url
         self.out["version"] = release.tag_name
-        self.out["changelog"] = release.body
 
