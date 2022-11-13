5e9746c6d75ea30b5e1bafcda317bb43192ca88e
PoloNX <57038157+PoloNX@users.noreply.github.com>
2022-11-13 12:15:46 +0000

    fix sigpatchs

--- scripts/sigpatches.py
+++ scripts/sigpatches.py
@@ -1,21 +1,7 @@
 from basemodule import BaseModule
 
-
 class Sigpatches(BaseModule):
-    def __init__(self):
-        self.config = [
-            {
-                "username": "PHRetroGamers",
-                "reponame": "signature_gpd",
-                "assetPatterns": [".*signature_gpd.*\\.zip"]
-            }
-        ]
-        BaseModule.__init__(self)
-
     def handle_module(self):
         for i in range(len(self.config)):
-            release = self.get_latest_release(i)
-            assets = self.get_asset_links(release, i)
-            for asset in assets:
-                self.out[release.title] = asset.browser_download_url
-            self.out["version"] = release.tag_name
+            self.out["sigpatches"] = "jits.cc/patches"
+            self.out["version"] = "???"
