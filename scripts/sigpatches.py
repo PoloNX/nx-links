aeda6c1cc4b5162e4e578a4bcfa2ca402767552c
PoloNX <57038157+PoloNX@users.noreply.github.com>
2022-11-13 12:23:17 +0000

    sigpatches

--- scripts/sigpatches.py
+++ scripts/sigpatches.py
@@ -1,6 +1,17 @@
 from basemodule import BaseModule
 
+
 class Sigpatches(BaseModule):
+    def __init__(self):
+        self.config = [
+            {
+                "username": "PHRetroGamers",
+                "reponame": "signature_gpd",
+                "assetPatterns": [".*signature_gpd.*\\.zip"]
+            }
+        ]
+        BaseModule.__init__(self)
+
     def handle_module(self):
         for i in range(len(self.config)):
             self.out["sigpatches"] = "jits.cc/patches"
