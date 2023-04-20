c347857481ca2496743236e42a27abd1d08dc857
PoloNX <geyrecharlie@gmail.com>
2023-04-20 13:56:46 +0200

    add pre release for goldleaf

--- scripts/homebrew.py
+++ scripts/homebrew.py
@@ -12,7 +12,7 @@ class Homebrew(BaseModule):
         BaseModule.__init__(self)
 
     def handle_module(self):
-        release = self.get_latest_release(0)
+        release = self.get_latest_pre_release(0)
         asset = self.get_asset_link(release, self.config[0]["assetPatterns"][0])
         self.out["Goldleaf"] = asset[0].browser_download_url
         self.out["Goldleaf_version"] = release.tag_name
