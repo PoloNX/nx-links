2ddd4abf2f04cfe674bcbaa684fb7379d2811b70
PoloNX <geyrecharlie@gmail.com>
2023-04-28 15:39:33 +0200

    add sysmodules

--- scripts/homebrew.py
+++ scripts/homebrew.py
@@ -13,20 +13,25 @@ class Homebrew(BaseModule):
 
     def handle_module(self):
         a=[]
-        a.append([{"username": "rashevskyv","reponame": "dbi","assetPatterns": [".*DBI.*\\.nro"]}])
-        a.append([{"username": "WerWolv","reponame": "EdiZon","assetPatterns": [".*EdiZon.*\\.nro"]}])
-        a.append([{"username": "mtheall","reponame": "ftpd","assetPatterns": [".*ftpd-classic.*\\.nro"]}])
-        a.append([{"username": "XorTroll","reponame": "Goldleaf", "assetPatterns": [".*Goldleaf.*\\.nro"]}])
-        a.append([{"username": "WerWolv","reponame": "Hekate-Toolbox","assetPatterns": [".*HekateToolbox.*\\.nro"]}])
-        a.append([{"username": "J-D-K","reponame": "JKSV","assetPatterns": [".*JKSV.*\\.nro"]}])
-        a.append([{"username": "tallbl0nde","reponame": "NX-Activity-Log","assetPatterns": [".*NX-Activity-Log.*\\.nro"]}])
-        a.append([{"username": "PoloNX","reponame": "Ls-News","assetPatterns": [".*Ls-News.*\\.nro"]}])
+        a.append([{"username": "rashevskyv","reponame": "dbi", "homebrew": True,"assetPatterns": [".*DBI.*\\.nro"]}])
+        a.append([{"username": "WerWolv","reponame": "EdiZon", "homebrew": True,"assetPatterns": [".*EdiZon.*\\.nro"]}])
+        a.append([{"username": "mtheall","reponame": "ftpd", "homebrew": True,"assetPatterns": [".*ftpd-classic.*\\.nro"]}])
+        a.append([{"username": "XorTroll","reponame": "Goldleaf", "homebrew": True,"assetPatterns": [".*Goldleaf.*\\.nro"]}])
+        a.append([{"username": "WerWolv","reponame": "Hekate-Toolbox", "homebrew": True,"assetPatterns": [".*HekateToolbox.*\\.nro"]}])
+        a.append([{"username": "J-D-K","reponame": "JKSV", "homebrew": True,"assetPatterns": [".*JKSV.*\\.nro"]}])
+        a.append([{"username": "tallbl0nde","reponame": "NX-Activity-Log", "homebrew": True,"assetPatterns": [".*NX-Activity-Log.*\\.nro"]}])
+        a.append([{"username": "PoloNX","reponame": "Ls-News", "homebrew": True,"assetPatterns": [".*Ls-News.*\\.nro"]}])
+
+        a.append([{"username": "ndeadly","reponame": "MissionControl", "homebrew": False,"assetPatterns": [".*MissionControl.*\\.zip"]}])
+        a.append([{"username": "exelix11","reponame": "SysDVR", "homebrew": False,"assetPatterns": ["SysDVR.zip"]}])
+        a.append([{"username": "WerWolv","reponame": "nx-ovlloader", "homebrew": False,"assetPatterns": [".*nx-ovlloader.*\\.zip"]}])
+        a.append([{"username": "XorTroll","reponame": "emuiibo", "homebrew": False,"assetPatterns": [".*emuiibo.*\\.zip"]}])
 
         for i in a:
             self.config = i
-            if self.config[0]["reponame"] == "Goldleaf":
+            if self.config[0]["reponame"] == "Goldleaf" or self.config[0]["reponame"] == "emuiibo":
                 release = self.get_latest_pre_release(0)
             else:
                 release = self.get_latest_release(0)
             asset = self.get_asset_link(release, self.config[0]["assetPatterns"][0])
-            self.out[self.config[0]["reponame"]] = {"name": i[0]["reponame"] , "link": asset[0].browser_download_url, "version": release.tag_name}
+            self.out[self.config[0]["reponame"]] = {"name": i[0]["reponame"] , "link": asset[0].browser_download_url, "version": release.tag_name, "homebrew": i[0]["homebrew"]}
